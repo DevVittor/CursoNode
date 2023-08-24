eb37b5a9d9f9d9d24241cefd63e5d2a6e4519d2d
Vittor fonseca serra <vittorserradev@gmail.com>
2023-08-24 15:32:19 -0300

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+require('dotenv').config();
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Olá Mundo!");
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}`);
+})
